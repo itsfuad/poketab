7326cd17427568d21b2ea0d440150dd1b5a5eda2
Fuad Hasan <49100463+itsfuad@users.noreply.github.com>
2022-03-06 11:08:44 +0600

    emoji varification

--- public/js/chat.js
+++ public/js/chat.js
@@ -77,6 +77,11 @@ socket.on('newMessage', function (message) {
   //pop.play();
   html = html.replace(/¶/g ,'<br>');
   jQuery('#messages').append(html);
+  console.log(emo_test(message.text));
+  if (emo_test(message.text))
+  {
+    $( "#messages li:last div p" ).css({"background": "none", "font-size": "30px", "padding": "0px"});
+  }
   updateScroll();
 });
 
@@ -94,6 +99,11 @@ socket.on('my__message', function (message) {
   //html = linkify(html);
   //console.log(html);
   jQuery('#messages').append(html);
+  console.log(emo_test(message.text));
+  if (emo_test(message.text))
+  {
+    $( "#messages li:last div p" ).css({"background": "none", "font-size": "30px", "padding": "0px"});
+  }
   updateScroll();
 });
 
@@ -221,6 +231,12 @@ function linkify(inputText) {
   replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
   replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
   return replacedText;
+}
+
+var emoji_regex = /^(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])+$/;
+
+function emo_test(str){
+  return emoji_regex.test(str);
 }
 
 /*
