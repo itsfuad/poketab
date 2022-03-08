57c7b168494020da53296e6f777f2f3fd1bc56d3
Fuad Hasan <49100463+itsfuad@users.noreply.github.com>
2022-03-08 21:25:06 +0600

    icon change

--- public/index.js
+++ public/index.js
@@ -27,7 +27,7 @@ if ('serviceWorker' in navigator){
     
     window.addEventListener('load', () => {
         navigator.serviceWorker
-        .register('sw-chatmate.js')
+        .register('sw-poketab.js')
         .then(reg => console.log("Service Worker Registered"))
         .catch(err => console.log(`Service Worker: Error ${err}`));
     });
