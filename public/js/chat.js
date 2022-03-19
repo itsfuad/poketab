bdde14f2b6d302afcc68ebeb5ad5f9e474819379
Fuad Hasan <49100463+itsfuad@users.noreply.github.com>
2022-03-19 23:27:46 +0600

    reply substring and cursor

--- public/js/chat.js
+++ public/js/chat.js
@@ -370,7 +370,7 @@ $('#messages').on('click', function (evt) {
   if (target.className === 'textMessage'){
     targetId = target.parentElement.parentElement.parentElement.id;
     //trim target text to 10 charecters
-    replyText =  `${target.innerText.substring(0, 300)}...`;
+    replyText =  `${target.innerText.substring(0, 200)} ...`;
     replyTo = evt.target.parentElement.previousElementSibling.previousElementSibling.innerText;
     replyTo = replyTo.replace(/ replied to [a-zA-Z]+/g, '');
     let replyToPop = replyTo;
@@ -379,7 +379,7 @@ $('#messages').on('click', function (evt) {
     isReply = true;
     $('.toast-popup').show();
     $('.toast-popup-name').text(`Replying to ${replyToPop}`);
-    $('.toast-popup-message').text(`${target.innerText.substring(0, 100)}`);
+    $('.toast-popup-message').text(`${target.innerText.substring(0, 50)} ...`);
     $('#textbox').focus();
   }
   else if (target.className.includes('replyMessage')){
