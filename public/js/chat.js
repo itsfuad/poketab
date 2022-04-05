c31ab6306ce68363655352493610297eb1ef6b45
Fuad Hasan <49100463+itsfuad@users.noreply.github.com>
2022-04-05 12:25:44 +0600

    #appHeight

--- public/js/chat.js
+++ public/js/chat.js
@@ -251,7 +251,7 @@ socket.on('deleteImage', (messageId, user) => {
 
 
 //functions
-function appHeigh () {
+function appHeight () {
   const doc = document.documentElement;
   doc.style.setProperty('--app-height', `${window.innerHeight}px`);
 }
