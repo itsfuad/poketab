193d9d2ad47f4ac096f3830cafd9ff2389c3fc06
Fuad Hasan <49100463+itsfuad@users.noreply.github.com>
2022-05-08 18:31:42 +0600

    :)

---
+++ server/utils/users.js
@@ -0,0 +1,73 @@
+[{
+  id: '/#12poiajdspfoif',
+  name: 'Uzair_Capazyte',
+  key: 'The Office Fans'
+}]
+
+// addUser(id, name, key)
+// removeUser(id)
+// getUser(id)
+// getUserList(key)
+
+class Users {
+  constructor () {
+    this.users = [];
+    this.MaxUser = new Map();
+  }
+  addUser (id, name, key, avatar, maxuser) {
+    this.MaxUser.set(key, maxuser);
+    //console.log(`Maxuser: ${maxuser}`);
+    var user = {id, name, key, avatar};
+    this.users.push(user);
+    return user;
+  }
+  removeUser (id) {
+    var user = this.getUser(id);
+
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id);
+    }
+
+    return user;
+  }
+  getUser (id) {
+    return this.users.filter((user) => user.id === id)[0]
+  }
+  getUserList (key) {
+    var users = this.users.filter((user) => user.key === key);
+    var namesArray = users.map((user) => user.name);
+    return namesArray;
+  }
+  getAvatarList(key){
+    var users = this.users.filter((user) => user.key === key);
+    var avatarArray = users.map((user) => user.avatar);
+    return avatarArray;
+  }
+  getMaxUser(key){
+    return this.MaxUser.get(key);
+  }
+  removeMaxUser(key){
+    this.MaxUser.delete(key);
+  }
+  getUserId(key){
+    var users = this.users.filter((user) => user.key === key);
+    var idArray = users.map((user) => user.id);
+    return idArray;
+  }
+}
+
+module.exports = {Users};
+
+ // class Person {
+ //   constructor (name, age) {
+ //     this.name = name;
+ //     this.age = age;
+ //   }
+ //   getUserDescription () {
+ //     return `${this.name} is ${this.age} year(s) old.`;
+ //   }
+ // }
+ //
+ // var me = new Person('Andrew', 25);
+ // var description = me.getUserDescription();
+ // console.log(description);
