84519e0362e0ec1b2e11720e1dbc18f44a9916f5
Fuad Hasan <49100463+itsfuad@users.noreply.github.com>
2022-05-08 18:41:18 +0600

    clean

--- server/utils/validation.js
+++
@@ -1,9 +0,0 @@
-var isRealString = (str) => {
-  return typeof str === 'string' && str.trim().length > 0;
-};
-function validateUserName(username){
-  const name_format = /^[a-zA-Z]{3,20}$/;
-  return (name_format.test(username) && username.trim().length > 0);
-}
-
-module.exports = {isRealString, validateUserName};
